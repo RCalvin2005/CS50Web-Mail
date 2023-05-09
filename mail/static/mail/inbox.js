a2cdf32468326f3a2f98ac7adaa3c633f77a660e
Richard Calvin Yong <r.calvin.yong@gmail.com>
2023-05-09 21:48:38 +0700

    Added send mail functionality

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -6,6 +6,9 @@ document.addEventListener('DOMContentLoaded', function() {
   document.querySelector('#archived').addEventListener('click', () => load_mailbox('archive'));
   document.querySelector('#compose').addEventListener('click', compose_email);
 
+  // Sending mail event listener
+  document.querySelector('#compose-form').onsubmit = send_mail;
+
   // By default, load the inbox
   load_mailbox('inbox');
 });
@@ -15,6 +18,10 @@ function compose_email() {
   // Show compose view and hide other views
   document.querySelector('#emails-view').style.display = 'none';
   document.querySelector('#compose-view').style.display = 'block';
+
+  // Hide error alert and clear out existing alert
+  document.querySelector('#error-alert').style.display = 'none';
+  document.querySelector('#error-alert').innerHTML = '';
 
   // Clear out composition fields
   document.querySelector('#compose-recipients').value = '';
@@ -30,4 +37,47 @@ function load_mailbox(mailbox) {
 
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+}
+
+function send_mail() {
+
+  // Send mail to server
+  fetch('/emails', {
+    method: 'POST',
+    body: JSON.stringify({
+        recipients: document.querySelector('#compose-recipients').value,
+        subject: document.querySelector('#compose-subject').value,
+        body: document.querySelector('#compose-body').value
+    })
+  })
+  .then(response => response.json())
+  .then(result => {
+
+    // Catch any errors
+    if (result.error) {
+
+      console.log("Error:", result.error);
+
+      // Display error alert
+      const alert = document.querySelector('#error-alert');
+      alert.innerHTML = result.error;
+      alert.style.display = 'block';
+
+      // https://stackoverflow.com/questions/4210798/how-to-scroll-to-top-of-page-with-javascript-jquery
+      // Scroll back to top
+      document.body.scrollTop = document.documentElement.scrollTop = 0;
+    }
+    else {
+      
+      console.log(result.message);
+
+      // Load sent mailbox if sent successfully
+      load_mailbox('sent');
+      
+      window.alert(result.message);
+    }
+  });
+
+  // Prevent default form submission
+  return false;
 }
